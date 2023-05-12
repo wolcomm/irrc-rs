16137f21e93c6602520fe1f5feeaf54d837f4b2e
Ben Maddison <benm@workonline.africa>
2023-05-12 23:53:44 +0200

    strict linting

--- src/lib.rs
+++ src/lib.rs
@@ -37,7 +37,47 @@
 //!
 //! [irrd]: https://irrd.readthedocs.io/en/stable/users/queries/#irrd-style-queries
 #![doc(html_root_url = "https://docs.rs/irrc/0.1.0-rc.4")]
+// clippy lints
+#![warn(clippy::pedantic)]
+#![warn(clippy::cargo)]
+#![warn(clippy::nursery)]
+#![allow(clippy::redundant_pub_crate)]
+// rustc lints
+#![allow(box_pointers)]
+#![warn(absolute_paths_not_starting_with_crate)]
+#![warn(deprecated_in_future)]
+#![warn(elided_lifetimes_in_paths)]
+#![warn(explicit_outlives_requirements)]
+#![warn(keyword_idents)]
+#![warn(macro_use_extern_crate)]
+#![warn(meta_variable_misuse)]
+#![warn(missing_abi)]
+#![warn(missing_copy_implementations)]
+#![warn(missing_debug_implementations)]
 #![warn(missing_docs)]
+#![warn(non_ascii_idents)]
+#![warn(noop_method_call)]
+#![warn(pointer_structural_match)]
+#![warn(rust_2021_incompatible_closure_captures)]
+#![warn(rust_2021_incompatible_or_patterns)]
+#![warn(rust_2021_prefixes_incompatible_syntax)]
+#![warn(rust_2021_prelude_collisions)]
+#![warn(single_use_lifetimes)]
+#![warn(trivial_casts)]
+#![warn(trivial_numeric_casts)]
+#![warn(unreachable_pub)]
+#![warn(unsafe_code)]
+#![warn(unsafe_op_in_unsafe_fn)]
+#![warn(unstable_features)]
+#![warn(unused_crate_dependencies)]
+#![warn(unused_extern_crates)]
+#![warn(unused_import_braces)]
+#![warn(unused_lifetimes)]
+#![warn(unused_qualifications)]
+#![warn(unused_results)]
+#![warn(variant_size_differences)]
+// docs.rs build config
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 
 mod client;
 mod parse;
