4b41fa8ed28d146d974542bbd45b433899479c1c
Ben Maddison <benm@workonline.africa>
2024-02-18 15:11:53 +0000

    bump version

--- src/lib.rs
+++ src/lib.rs
@@ -35,7 +35,7 @@
 //! ```
 //!
 //! [irrd]: https://irrd.readthedocs.io/en/stable/users/queries/#irrd-style-queries
-#![doc(html_root_url = "https://docs.rs/irrc/0.1.0-rc.7")]
+#![doc(html_root_url = "https://docs.rs/irrc/0.1.0")]
 // clippy lints
 #![warn(clippy::pedantic)]
 #![warn(clippy::cargo)]
